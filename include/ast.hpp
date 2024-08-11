b1b701dbf8bfa868b7eadb0deceafe308cd71452
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-11 22:48:09 +0545

    update ast header

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,48 +1,150 @@
-#ifndef AST_HPP
-#define AST_HPP
+#ifndef AST_H
+#define AST_H
 
-#include <iostream>
+#include <string>
 #include <vector>
 
-enum class ASTNodeType {
-    PROGRAM,
-    STATEMENT_LIST,
-    BHANA_STMT,
-    SUNA_STMT,
-    JABASAMMA_LOOP,
-    LAGI_LOOP,
-    YADI_STMT,
-    ANYATHA_STMT,
-    KAAM_DECL,
-    FIRTA_STMT,
-    PURNADATA_TYPE,
-    DASADATA_TYPE,
-    AKSHAR_DATA_TYPE,
-    PAATH_DATA_TYPE,
-    VARIABLE,
-    EXPRESSION,
-    TERM,
-    FACTOR,
-    NUMBER,
-    STRING,
-    UNKNOWN
-};
-
+// Base class for all AST nodes
 class ASTNode {
 public:
-    ASTNode(ASTNodeType type, std::string value);
-    virtual ~ASTNode();
-
-    void addChild(ASTNode* child);
-    void print(int indent);
-
-    ASTNodeType getType() { return type; }
-    std::string getValue() { return value; }
-
-private:
-    ASTNodeType type;
-    std::string value;
-    std::vector<ASTNode*> children;
+    virtual ~ASTNode() = default;
 };
 
-#endif  // AST_HPP
+// Node for variable declarations and assignments
+class VariableDeclarationNode : public ASTNode {
+public:
+    std::string type;
+    std::string variableName;
+    ASTNode* initialValue;  // Can be null if no initial value is assigned
+
+    VariableDeclarationNode(const std::string& type, const std::string& name, ASTNode* value = nullptr)
+        : type(type), variableName(name), initialValue(value) {}
+};
+
+class AssignmentNode : public ASTNode {
+public:
+    std::string variableName;
+    ASTNode* value;
+
+    AssignmentNode(const std::string& name, ASTNode* value)
+        : variableName(name), value(value) {}
+};
+
+// Node for print statements
+class PrintNode : public ASTNode {
+public:
+    ASTNode* expression;
+
+    PrintNode(ASTNode* expr) : expression(expr) {}
+};
+
+// Node for input statements
+class InputNode : public ASTNode {
+public:
+    std::string variableName;
+
+    InputNode(const std::string& name) : variableName(name) {}
+};
+
+// Node for if statements
+class IfNode : public ASTNode {
+public:
+    ASTNode* condition;
+    ASTNode* thenBlock;
+    ASTNode* elseBlock;  // Can be null if no else block
+
+    IfNode(ASTNode* cond, ASTNode* thenBlk, ASTNode* elseBlk = nullptr)
+        : condition(cond), thenBlock(thenBlk), elseBlock(elseBlk) {}
+};
+
+// Node for while statements
+class WhileNode : public ASTNode {
+public:
+    ASTNode* condition;
+    ASTNode* block;
+
+    WhileNode(ASTNode* cond, ASTNode* blk) : condition(cond), block(blk) {}
+};
+
+// Node for for statements
+class ForNode : public ASTNode {
+public:
+    ASTNode* initStatement;
+    ASTNode* condition;
+    ASTNode* increment;
+    ASTNode* block;
+
+    ForNode(ASTNode* init, ASTNode* cond, ASTNode* incr, ASTNode* blk)
+        : initStatement(init), condition(cond), increment(incr), block(blk) {}
+};
+
+// Node for function definitions
+class FunctionDefinitionNode : public ASTNode {
+public:
+    std::string returnType;  // Use "khali" for void functions
+    std::string functionName;
+    std::vector<VariableDeclarationNode*> parameters;
+    ASTNode* body;
+
+    FunctionDefinitionNode(const std::string& returnType, const std::string& name,
+                           const std::vector<VariableDeclarationNode*>& params, ASTNode* body)
+        : returnType(returnType), functionName(name), parameters(params), body(body) {}
+};
+
+// Node for return statements
+class ReturnNode : public ASTNode {
+public:
+    ASTNode* value;  // Can be null if returning nothing
+
+    ReturnNode(ASTNode* val = nullptr) : value(val) {}
+};
+
+// Node for expressions
+class ExpressionNode : public ASTNode {
+public:
+    virtual ~ExpressionNode() = default;
+};
+
+// Concrete expression nodes
+class LiteralNode : public ExpressionNode {
+public:
+    std::string value;
+
+    LiteralNode(const std::string& val) : value(val) {}
+};
+
+class IdentifierNode : public ExpressionNode {
+public:
+    std::string name;
+
+    IdentifierNode(const std::string& name) : name(name) {}
+};
+
+class BinaryOperationNode : public ExpressionNode {
+public:
+    ExpressionNode* left;
+    std::string op;  // Operator (e.g., "+", "-", "*", "/")
+    ExpressionNode* right;
+
+    BinaryOperationNode(ExpressionNode* left, const std::string& op, ExpressionNode* right)
+        : left(left), op(op), right(right) {}
+};
+
+class UnaryOperationNode : public ExpressionNode {
+public:
+    std::string op;  // Operator (e.g., "-", "!")
+    ExpressionNode* operand;
+
+    UnaryOperationNode(const std::string& op, ExpressionNode* operand)
+        : op(op), operand(operand) {}
+};
+
+// Node for blocks of statements
+class BlockNode : public ASTNode {
+public:
+    std::vector<ASTNode*> statements;
+
+    BlockNode(const std::vector<ASTNode*>& stmts) : statements(stmts) {}
+};
+
+#endif // AST_H
