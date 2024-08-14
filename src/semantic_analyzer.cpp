afdce4ad5145f6a6d511853620a81d36ebc77f43
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-15 00:23:43 +0545

    complete the semantic checker

--- src/semantic_analyzer.cpp
+++ src/semantic_analyzer.cpp
@@ -1,19 +1,102 @@
-#ifndef SEMANTIC_ANALYZER_H
-#define SEMANTIC_ANALYZER_H
+#include <stdexcept>
+#include "semantic_analyzer.hpp"
 
-#include "ast.hpp"
-#include "symbol_table.hpp"
-#include <string>
+void SemanticAnalyzer::analyzeNode(ASTNode* node) {
+    if (auto* program = dynamic_cast<Program*>(node)) {
+        for (auto* n : program->nodes) {
+            analyzeNode(n);
+        }
+    } else if (auto* declaration = dynamic_cast<Declaration*>(node)) {
+        analyzeDeclaration(declaration);
+    } else if (auto* statement = dynamic_cast<Statement*>(node)) {
+        analyzeStatement(statement);
+    } else if (auto* expression = dynamic_cast<Expression*>(node)) {
+        analyzeExpression(expression);
+    }
+}
 
-class SemanticAnalyzer{
-    SymbolTable symbolTable;
+void SemanticAnalyzer::analyzeStatement(Statement *stmt){
+    if(auto* printStmt = dynamic_cast<PrintStatement*>(stmt)){
+        analyzeExpression(printStmt->expr);
+    } else if(auto* assignmentStmt = dynamic_cast<AssignmentStatement*>(stmt)) {
+        // Check if the variable is declared
+        symbolTable.lookupVariable(assignmentStmt->varName);
+        analyzeExpression(assignmentStmt->expr);
+    } else if (auto* ifStmt = dynamic_cast<IfStatement*>(stmt)) {
+        analyzeExpression(ifStmt->condition);
+        analyzeStatement(ifStmt->thenBranch);
+        if (ifStmt->elseBranch) {
+            analyzeStatement(ifStmt->elseBranch);
+        }
+    } else if (auto* whileStmt = dynamic_cast<WhileStatement*>(stmt)) {
+        analyzeExpression(whileStmt->condition);
+        analyzeStatement(whileStmt->body);
+    } else if (auto* forStmt = dynamic_cast<ForStatement*>(stmt)) {
+        if (forStmt->initializer) {
+            analyzeStatement(forStmt->initializer);
+        }
+        analyzeExpression(forStmt->condition);
+        if (forStmt->increment) {
+            analyzeExpression(forStmt->increment);
+        }
+        analyzeStatement(forStmt->body);
+    } else if (auto* blockStmt = dynamic_cast<BlockStatement*>(stmt)) {
+        for (auto* s : blockStmt->statements) {
+            analyzeStatement(s);
+        }
+    }
+}
 
-    void analyzeStatement(Statement*);
-    void analyzeExpression(Expression*);
-    void checkType(Expression* , const std::string&);
+void SemanticAnalyzer::analyzeExpression(Expression* expr){
+    if(auto* variable = dynamic_cast<Variable*>(expr)) {
+        // Check if varibale is declared
+        symbolTable.lookupVariable(variable->name);
+    } else if (auto* binaryOp = dynamic_cast<BinaryOperation*>(expr)) {
+        analyzeExpression(binaryOp->left);
+        analyzeExpression(binaryOp->right);
+    } else if (auto* unaryOp = dynamic_cast<UnaryOperation*>(expr)) {
+        analyzeExpression(unaryOp->expr);
+    }
+}
 
-    public:
-        void analyze(ASTNode* root);
-};
+void SemanticAnalyzer::analyzeDeclaration(Declaration* decl){
+    if(auto* varDecl = dynamic_cast<VariableDeclaration*>(decl)){
+        if(varDecl->type == VariableDeclaration::VOID){
+            throw std::runtime_error("Variable cannot have void type");
+        }
+        symbolTable.declareVariable(varDecl->varName, static_cast<VarType>(varDecl->type));
+        
+    } else if(auto * funcDecl = dynamic_cast<FunctionDeclaration*>(decl)){
+        try {
+            symbolTable.lookupVariable(funcDecl->name);
+            throw std::runtime_error("Function " + funcDecl->name + " already declared!");
+        } catch (const std::runtime_error&){
+            // nothing...
+        }
 
-#endif
+        symbolTable.declareVariable(funcDecl->name, VarType::Khali);
+
+        for (auto* param : funcDecl->parameters) {
+            if (param->type == VariableDeclaration::VOID) {
+                throw std::runtime_error("Function parameter cannot have VOID type.");
+            }
+        }
+
+        if (auto* returnTypeDecl = dynamic_cast<VariableDeclaration*>(funcDecl->returnType)) {
+            if (returnTypeDecl->type == VariableDeclaration::VOID) {
+                throw std::runtime_error("Function must have a non-VOID return type.");
+            }
+        }
+    }
+}
+
+void SemanticAnalyzer::checkType(Expression* expr, VarType expectedType){
+
+}
+
+void SemanticAnalyzer::analyze(ASTNode* root){
+    if(root){
+        analyzeNode(root);
+    }
+}
+
