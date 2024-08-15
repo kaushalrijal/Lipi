2b2a945fa4036425c99d3ed6acac706e7241a53d
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-15 22:51:39 +0545

    add basic main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "lexer.hpp"
+#include "parser.hpp"
+#include "semantic_analyzer.hpp"
+
+int main(int argc, char** argv){
+    if(argc<2){
+        std::cerr << "Arguments list cannot be empty.";
+    }
+
+    return 0;
+}
