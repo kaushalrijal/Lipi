bf29db24f467fd37e65c637e5c38a52b3c66476c
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-16 21:03:05 +0545

    create code generator header

---
+++ include/code_generator.hpp
@@ -0,0 +1,17 @@
+#ifndef CODE_GENERATOR_HPP
+#define CODE_GENERATOR_HPP
+
+#include "ast.hpp"
+#include <string>
+
+class CodeGenerator {
+public:
+    std::string generate(ASTNode* root); // Main method to generate code from AST
+
+private:
+    std::string generateExpression(Expression* expr);
+    std::string generateStatement(Statement* stmt);
+    std::string generateDeclaration(Declaration* decl);
+};
+
+#endif
