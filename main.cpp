60152a66c2fa36e3c65b3bd263483f4967fad1f2
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-25 23:24:57 +0545

    minor tweaks

--- main.cpp
+++ main.cpp
@@ -2,33 +2,31 @@
 #include <iostream>
 #include <vector>
 
-#include "lexer.hpp"
-// #include "parser.hpp"
-// #include "semantic_analyzer.hpp"
+
 
 int main(int argc, char** argv){
-    if(argc<2){
-        std::cerr << "Arguments list cannot be empty.";
-    }
+    // if(argc<2){
+    //     std::cerr << "Arguments list cannot be empty.";
+    // }
     
-    std::ifstream inf(argv[1]);
+    // std::ifstream inf(argv[1]);
 
-    if(inf.fail()){
-        std::cerr << "Failed to open file " << argv[1];
-    }
+    // if(inf.fail()){
+    //     std::cerr << "Failed to open file " << argv[1];
+    // }
 
-    char ch[1000];
+    // char ch[1000];
 
-    std::string code;
+    // std::string code;
     
-    while(inf.eof()!=true){
-        inf.getline(ch, 1000);
-        code.append(ch);
-    } 
+    // while(inf.eof()!=true){
+    //     inf.getline(ch, 1000);
+    //     code.append(ch);
+    // } 
 
-    Lexer lexer(code);
-    std::vector<Token> tokens = lexer.tokenize();
+    // cout << code;
 
+    std::cout << "ITs' workign";
 
     return 0;
 }
